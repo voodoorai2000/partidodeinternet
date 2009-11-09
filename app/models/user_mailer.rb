dd4a6bef8d49b8239485e737ee5fbb53c12b1194
rgarcia <voodoorai2000@gmail.com>
2009-11-09 19:27:02 +0100

    correct host for urls in emails

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,16 +1,17 @@
 class UserMailer < ActionMailer::Base
+  
   def signup_notification(user)
     setup_email(user)
     @subject    += 'Bienvenido a la tribu'
   
-    @body[:url]  = "http://iweekend.raimondgarcia/activate/#{user.activation_code}"
+    @body[:url]  = "#{host}/activate/#{user.activation_code}"
   
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://iweekend.raimondgarcia/"
+    @body[:url]  = "#{host}"
   end
   
   protected
@@ -20,5 +21,9 @@ class UserMailer < ActionMailer::Base
       @subject     = ""
       @sent_on     = Time.now
       @body[:user] = user
+    end
+    
+    def host
+      "http://iweekend.raimondgarcia.com"
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091026182655) do
+ActiveRecord::Schema.define(:version => 20091026184003) do
 
   create_table "emails", :force => true do |t|
     t.string   "from"
