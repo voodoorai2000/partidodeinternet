f79f4a07f8e5859d5f94fda5751aee40a0bd6b34
rgarcia <voodoorai2000@gmail.com>
2009-11-12 02:06:18 +0100

    encrypting password only on create

---
+++ db/migrate/20091112010401_not_storing_password.rb
@@ -0,0 +1,9 @@
+class NotStoringPassword < ActiveRecord::Migration
+  def self.up
+    remove_column :users, :password
+  end
+
+  def self.down
+    add_column :users, :password, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091112004343) do
+ActiveRecord::Schema.define(:version => 20091112010401) do
 
   create_table "emails", :force => true do |t|
     t.string   "from"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(:version => 20091112004343) do
     t.string   "url"
     t.integer  "region_id"
     t.boolean  "admin"
-    t.string   "password"
   end
 
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
