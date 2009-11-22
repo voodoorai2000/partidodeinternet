fa81ecc084bd179315196c5d4ffe9d5da0fea362
rgarcia <voodoorai2000@gmail.com>
2009-11-22 16:10:37 +0100

    correct host for sending emails in staging and production

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -3,15 +3,11 @@ class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
     @subject    += 'Bienvenido al Partido de Internet'
-  
-    @body[:url]  = "#{host}/activate/#{user.activation_code}"
-  
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Tu cuenta está activada'
-    @body[:url]  = "#{host}"
   end
   
   protected
@@ -21,9 +17,5 @@ class UserMailer < ActionMailer::Base
       @subject     = ""
       @sent_on     = Time.now
       @body[:user] = user
-    end
-    
-    def host
-      "http://iweekend.raimondgarcia.com"
     end
 end
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -20,7 +20,7 @@
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+   config.action_mailer.default_url_options = { :host => "http://iweekend.raimondgarcia.com" }
 
   # Disable delivery errors, bad email addresses will be ignored
    config.action_mailer.raise_delivery_errors = true
