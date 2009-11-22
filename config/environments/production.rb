3526866a59e2ab7a9afebc504150b4448b0f9923
rgarcia <voodoorai2000@gmail.com>
2009-11-22 19:25:16 +0100

    fixing hosts in emails being send

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ config.action_view.cache_template_loading            = true
 # config.cache_store = :mem_cache_store
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
- config.action_mailer.default_url_options = { :host => "http://registro.partidodeinternet.es" }
+ config.action_mailer.default_url_options = { :host => "registro.partidodeinternet.es" }
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
